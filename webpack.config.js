382bc1154708a87a28efc706d50f000a022e508a
Ilya Volkov <volkov-ilyha@ya.ru>
2021-06-22 16:27:55 +0300

    Подготовка к началу проекта

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    watchContentBase: true,
+    open: true,
+    overlay: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: 'ts-loader'
+        }
+      }
+    ]
+  }
+}
