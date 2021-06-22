0cce19a4621b0cda1fc6fe9a61b1ec7b0c87b0de
Ilya Volkov <volkov-ilyha@ya.ru>
2021-06-22 18:33:12 +0300

    Добавляет кнопки "Поробнее" и "Удалить"

--- src/components/BookItem/index.tsx
+++ src/components/BookItem/index.tsx
@@ -6,6 +6,12 @@ interface IBookItemProps {
 
 export const BookItem: React.FC<IBookItemProps> = ({ name } ) => {
   return (
-    <li className="list-group-item list-group-item-action">{name}</li>
+    <li className="list-group-item list-group-item-action d-flex align-items-center">
+      {name}
+      <div className="ms-auto">
+        <a className="btn btn-primary me-3">Подробнее</a>
+        <button className="btn btn-danger">Удалить</button>
+      </div>
+    </li>
   )
 };
